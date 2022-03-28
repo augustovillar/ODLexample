44b52fea43fe3fa0c53ef9d0514a08c0451ab1d8
Augusto Villar <64085472+augustovillar@users.noreply.github.com>
2022-03-28 11:48:30 -0300

    Delete impl directory

--- impl/target/apidocs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"org.opendaylight.example.impl","c":"ExampleProvider","l":"close()"},{"p":"org.opendaylight.example.impl","c":"ExampleProvider","l":"ExampleProvider(DataBroker)","url":"%3Cinit%3E(org.opendaylight.controller.md.sal.binding.api.DataBroker)"},{"p":"org.opendaylight.example.impl","c":"ExampleProvider","l":"init()"}]
